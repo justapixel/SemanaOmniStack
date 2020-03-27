4517a8df1d8f5fe19ec8df807f8d37fa3977f997
Rhuan Oliveira <rhuan.dev@gmail.com>
2020-03-27 16:24:29 -0300

    feat: add  routes and login page

--- SemanaOmniStack11/frontend/src/App.js
+++ SemanaOmniStack11/frontend/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import Header from "./Header";
+
+import './global.css'
+import Routes from './routes'
+
 function App() {
   return (
-    <Header>Semana OmniStack</Header>
+    <Routes/>
   );
 }
 
