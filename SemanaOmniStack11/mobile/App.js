643dc07b5de3a5a513cb878805f397ea785b8d18
Rhuan Oliveira <rhuan.dev@gmail.com>
2020-03-28 15:55:40 -0300

    feat: add incidents view

--- SemanaOmniStack11/mobile/App.js
+++ SemanaOmniStack11/mobile/App.js
@@ -1,25 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+
+import Routes from './src/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Semana OmniStack</Text>
-    </View>
+    <Routes />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  title: {
-    color: '#FFF',
-    fontSize: 20,
-    fontWeight: 'bold'
-  }
-});
