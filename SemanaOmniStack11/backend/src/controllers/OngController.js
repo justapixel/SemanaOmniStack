65b6d502fa9f5b290d793cf16407606e53496e46
Rhuan Oliveira <rhuan.dev@gmail.com>
2020-03-29 15:39:55 -0300

    feat: add tests

--- SemanaOmniStack11/backend/src/controllers/OngController.js
+++ SemanaOmniStack11/backend/src/controllers/OngController.js
@@ -1,6 +1,5 @@
 const connection = require('../database/connection')
-const crypto = require('crypto')
-
+const generateUniqueId = require('../utils/generateUniqueId')
 module.exports = {
   async index (request, response) {
     const ongs = await connection('ongs').select('*');
@@ -10,9 +9,9 @@ module.exports = {
 
   async create(request, response) {
     const {name, email, whatsapp, city, uf} = request.body;
-
-    let id = crypto.randomBytes(4).toString('HEX');
     
+    const id = generateUniqueId();
+
     try {
       await connection('ongs').insert({
         id,
